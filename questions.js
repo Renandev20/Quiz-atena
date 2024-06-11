af057b68ea73060f49be4f9242190124e8d402eb
Renan Oliveira Nunes <156925993+Renandev20@users.noreply.github.com>
2024-06-11 12:54:08 -0300

    Add files via upload
    projeto

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question: "Qual é a fórmula para calcular a força resultante (F) de um objeto?",
+    answers: [
+      { option: "F = m * a", correct: true },
+      { option: "F = m + a", correct: false },
+      { option: "F = m / a", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a fórmula para calcular a energia cinética (E) de um objeto?",
+    answers: [
+      { option: "E = m * g * h", correct: false },
+      { option: "E = m * v²", correct: true },
+      { option: "E = m * a", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o processo químico de decomposição de compostos orgânicos pela ação de microrganismos?",
+    answers: [
+      { option: "Oxidação", correct: false },
+      { option: "Fermentação", correct: true },
+      { option: "Combustão", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o principal componente do ácido sulfúrico (H2SO4)?",
+    answers: [
+      { option: "Hidrogênio", correct: false },
+      { option: "Enxofre", correct: true },
+      { option: "Oxigênio", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o algoritmo de ordenação mais eficiente para uma grande quantidade de dados?",
+    answers: [
+      { option: "Bubble Sort", correct: false },
+      { option: "Insertion Sort", correct: false },
+      { option: "Quick Sort", correct: true },
+    ],
+  }
+];
